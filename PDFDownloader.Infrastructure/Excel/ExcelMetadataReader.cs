a107c508ce7f5133bc4c829c724aa97bab233652
TheSharpKillers <thomasbn2409@gmail.com>
2026-03-05 06:57:13 +0100

    Logic for ExcelMetadataReader

--- PDFDownloader.Infrastructure/Excel/ExcelMetadataReader.cs
+++ PDFDownloader.Infrastructure/Excel/ExcelMetadataReader.cs
@@ -1,4 +1,5 @@
-﻿using PDFDownloader.Core.Interfaces;
+﻿using ClosedXML.Excel;
+using PDFDownloader.Core.Interfaces;
 using PDFDownloader.Core.Models;
 
 namespace PDFDownloader.Infrastructure.Excel
@@ -23,6 +24,36 @@ namespace PDFDownloader.Infrastructure.Excel
         public Task<List<ReportMetadata>> ReadAsync()
         {
             List<ReportMetadata> reports = new List<ReportMetadata>();
+
+            // Open the Excel file
+            using XLWorkbook workbook = new XLWorkbook(_filePath);
+
+            // Gets the first sheet in the workbook
+            IXLWorksheet worksheet = workbook.Worksheet(1);
+
+            // RangeUsed() -> Returns the area that contains data
+            // RowUsed() -> Returns only rows that contains data
+            // Skip() -> Skips that amount of rows
+            IEnumerable<IXLRangeRow> rows = worksheet.RangeUsed()!.RowsUsed().Skip(_startRow - 1);
+
+            foreach (IXLRangeRow row in rows)
+            {
+                string brNummer = row.Cell(_brNummerColumn).GetString().Trim();
+                string primaryUrl = row.Cell(_primaryUrlColumn).GetString().Trim();
+                string secondaryUrl = row.Cell(_secondaryUrlColumn).GetString().Trim();
+
+                if (string.IsNullOrWhiteSpace(brNummer))
+                    continue;
+
+                // If there is no secondaryUrl in the Excel worksheet
+                // SecondaryUrl will be null, else it will be secondaryUrl
+                reports.Add(new ReportMetadata
+                {
+                    BRNummer = brNummer,
+                    PrimaryUrl = primaryUrl,
+                    SecondaryUrl = string.IsNullOrWhiteSpace(secondaryUrl) ? null : secondaryUrl
+                });
+            }
 
             return Task.FromResult(reports);
         }
